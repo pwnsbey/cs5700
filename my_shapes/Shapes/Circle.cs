43575d69105734765e3f4db043fd215e00fa76ba
pwnsbey <jake.ownsbey@gmail.com>
2018-09-11 15:34:03 -0600

    Added ellipses and circles

--- my_shapes/Shapes/Circle.cs
+++ my_shapes/Shapes/Circle.cs
@@ -4,8 +4,7 @@ namespace Shapes
 {
     public class Circle : ComplexShape
     {
-        public Point Center { get; private set; }
-        public double Radius { get; private set;  }
+        private Ellipse Ellipse;
 
         /**
          * Constructor with x-y Location for center
@@ -13,13 +12,10 @@ namespace Shapes
          * @param x                 The x-location of the center of the circle -- must be a valid double
          * @param y                 The y-location of the center of the circle
          * @param radius            The radius of the circle -- must be greater or equal to zero.
-         * @throws ShapeException   The exception thrown if the x, y, or z are not valid
          */
         public Circle(double x, double y, double radius)
         {
-            Validator.ValidatePositiveDouble(radius, "Invalid radius");
-            Center = new Point(x, y);
-            Radius = radius;
+            Ellipse = new Ellipse(x, y, radius, radius);
         }
 
         /**
@@ -27,27 +23,36 @@ namespace Shapes
          *
          * @param center            The x-location of the center of the circle -- must be a valid point
          * @param radius            The radius of the circle -- must be greater or equal to zero.
-         * @throws ShapeException   The exception thrown if the x, y, or z are not valid
          */
-        public Circle(Point center, double radius) {
-        
-            Validator.ValidatePositiveDouble(radius, "Invalid radius");
-            if (center == null)
-                throw new ShapeException("Invalid center point");
+        public Circle(Point center, double radius)
+        {
+            Ellipse = new Ellipse(center, radius, radius);
+        }
 
-            Center = center;
-            Radius = radius;
+        /**
+         * @return the center of the circle as a Point
+         */
+        public Point GetCenter()
+        {
+            return Ellipse.Center;
+        }
+
+        /*
+         * @return the radius of the circle
+         */
+        public double GetRadius()
+        {
+            return Ellipse.VertRadius;
         }
 
         /**
          * Move the circle
          * @param deltaX            a delta change for the x-location of center of the circle
          * @param deltaY            a delta change for the y-location of center of the circle
-         * @throws ShapeException   Exception thrown if either the delta x or y are not valid doubles
          */
         public override void Move(double deltaX, double deltaY)
         {
-            Center.Move(deltaX, deltaY);
+            Ellipse.Move(deltaX, deltaY);
         }
 
         /**
@@ -56,12 +61,10 @@ namespace Shapes
          * @param scaleFactor       a non-negative double that represents the percentage to scale the circle.
          *                          0>= and <1 to shrink.
          *                          >1 to grow.
-         * @throws ShapeException   Exception thrown if the scale factor is not valid
          */
         public void Scale(double scaleFactor)
         {
-            Validator.ValidatePositiveDouble(scaleFactor, "Invalid scale factor");
-            Radius *= scaleFactor;
+            Ellipse.Scale(scaleFactor);
         }
 
         /**
@@ -69,7 +72,7 @@ namespace Shapes
          */
         public override double ComputeArea()
         {
-            return Math.PI * Math.Pow(Radius, 2);
+            return Ellipse.ComputeArea();
         }
     }
 }
---
+++ my_shapes/Shapes/Ellipse.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Ellipse : ComplexShape
+    {
+        public Point Center { get; private set; }
+        public double VertRadius { get; private set;  }
+        public double HorizRadius { get; private set; }
+
+        /**
+         * Constructor with x-y Location for center
+         *
+         * @param x                 The x-location of the center of the ellipse -- must be a valid double
+         * @param y                 The y-location of the center of the ellipse -- must be a valid double
+         * @param vertRadius        The vertical radius of the ellipse -- must be greater or equal to zero.
+         * @param horizRadius       The horizontal radius of the ellipse -- must be greater or equal to zero.
+         * @throws ShapeException   The exception thrown if the x, y, or z are not valid
+         */
+        public Ellipse(double x, double y, double vertRadius, double horizRadius)
+        {
+            Validator.ValidatePositiveDouble(vertRadius, "Invalid vertical radius");
+            Validator.ValidatePositiveDouble(horizRadius, "Invalid horizontal radius");
+
+            Center = new Point(x, y);
+            VertRadius = vertRadius;
+            HorizRadius = horizRadius;
+        }
+
+        /**
+         * Constructor with a Point for center
+         *
+         * @param center            The Point location of the center of the ellipse -- must be a valid point
+         * @param vertRadius        The vertical radius of the ellipse -- must be greater or equal to zero.
+         * @param horizRadius       The horizontal radius of the ellipse -- must be greater or equal to zero.
+         * @throws ShapeException   The exception thrown if the x, y, or z are not valid
+         */
+        public Ellipse(Point center, double vertRadius, double horizRadius) {
+
+            Validator.ValidatePositiveDouble(vertRadius, "Invalid vertical radius");
+            Validator.ValidatePositiveDouble(horizRadius, "Invalid horizontal radius");
+            if (center == null)
+                throw new ShapeException("Invalid center point");
+
+            Center = center;
+            VertRadius = vertRadius;
+            HorizRadius = horizRadius;
+        }
+
+        /**
+         * Move the ellipse
+         * @param deltaX            a delta change for the x-location of center of the ellipse
+         * @param deltaY            a delta change for the y-location of center of the ellipse
+         * @throws ShapeException   Exception thrown if either the delta x or y are not valid doubles
+         */
+        public override void Move(double deltaX, double deltaY)
+        {
+            Center.Move(deltaX, deltaY);
+        }
+
+        /**
+         * Scale the ellipse
+         *
+         * @param scaleFactor       a non-negative double that represents the percentage to scale the ellipse.
+         *                          0>= and <1 to shrink.
+         *                          >1 to grow.
+         * @throws ShapeException   Exception thrown if the scale factor is not valid
+         */
+        public void Scale(double scaleFactor)
+        {
+            Validator.ValidatePositiveDouble(scaleFactor, "Invalid scale factor");
+            VertRadius  *= scaleFactor;
+            HorizRadius *= scaleFactor;
+        }
+
+        /**
+         * @return  The area of the ellipse.
+         */
+        public override double ComputeArea()
+        {
+            return Math.PI * VertRadius * HorizRadius;
+        }
+    }
+}
